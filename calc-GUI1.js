d9fdd46f1eed8bb60fde857af7b780a26ae7e9cf
Son <guswns1659@gmail.com>
2019-11-23 21:29:30 +0900

    계산기 버튼 GUI 구현하기.

---
+++ calc-GUI1.js
@@ -0,0 +1,8 @@
+var clickNumbers = function(event) {
+    console.log("click Numbers");
+    console.log(event.target.innerHTML);
+};
+var clickOthers = function(event) {
+    console.log("click Others");
+    console.log(event.target.innerHTML);
+};
